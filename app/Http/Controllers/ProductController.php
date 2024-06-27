70f2755f1e0b7d3a25e89a014559321b5c53965c
saim-x <x_aci@hotmail.com>
2024-06-27 21:08:45 +0500

    maam

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,10 +6,16 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('products.index');
     }
-    public function create(){
+    public function create()
+    {
         return view('products.create');
+    }
+    public function store(Request $request)
+    {
+        
     }
 }
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -7,6 +7,28 @@
 </head>
 <body>
     <h1>Create a Product</h1>
-    <form action=""  method="post"></form>
+    <form action="{{route('product.store')}}"  method="post">
+        @csrf
+        @method('post')
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name">
+        </div>
+        <div>
+            <label>Qty</label>
+            <input type="text" name="qty" placeholder="Qty">
+        </div>
+        <div>
+            <label>Price</label>
+            <input type="text" name="price" placeholder="Price">
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="text" name="description" placeholder="Description">
+        </div>
+        <div>
+            <input type="submit" value="Save a New Product">
+        </div>
+    </form>
 </body>
 </html>
